6e8fb942a5691aa97deff81410fb5b4ace696258
Coding <dev.tandpfun@gmail.com>
2022-03-07 12:00:57 -0800

    add display to server

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # Todo list storage
 global todoList
-todoList = []
+todoList = [
+  {
+    "name": "Go to school",
+    "completed": True,
+  }
+]
 
 # Import server from server file
 import server
+import display
 
 server.run()
+display.showItems(todoList)
