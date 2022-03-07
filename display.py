37344a1f03caf85aa33d71326a6442ca614a028b
Coding <dev.tandpfun@gmail.com>
2022-03-07 10:00:02 -0800

    fix positioning, add test items

--- display.py
+++ display.py
@@ -14,37 +14,6 @@ try:
 except TypeError:
   raise TypeError("You need to update the Inky library to >= v1.1.0")
 
-# Get the current path
-PATH = os.path.dirname(__file__)
-
-def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
-    """Create a transparency mask.
-    Takes a paletized source image and converts it into a mask
-    permitting all the colours supported by Inky pHAT (0, 1, 2)
-    or an optional list of allowed colours.
-    :param mask: Optional list of Inky pHAT colours to allow.
-    """
-    mask_image = Image.new("1", source.size)
-    w, h = source.size
-    for x in range(w):
-        for y in range(h):
-            p = source.getpixel((x, y))
-            if p in mask:
-                mask_image.putpixel((x, y), 255)
-
-    return mask_image
-
-# Dictionaries to store our icons and icon masks in
-icons = {}
-masks = {}
-
-# Load our icon files and generate masks
-for icon in glob.glob(os.path.join(PATH, "resources/*.png")):
-  icon_name = icon.split("resources/")[1].replace(".png", "")
-  icon_image = Image.open(icon)
-  icons[icon_name] = icon_image
-  masks[icon_name] = create_mask(icon_image)
-
 # Figure out scaling for display size
 scale_size = 1.0
 padding = 0
@@ -73,7 +42,6 @@ hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(16 * scale_size
 y_top = int(inky_display.height * (5.0 / 10.0))
 y_bottom = y_top + int(inky_display.height * (4.0 / 10.0))
 
-        
 # Calculate the positioning and draw the "Hello" text
 title_w, title_h = hanken_bold_font.getsize("TodoInky")
 # hello_x = int((inky_display.width - hello_w) / 2)
@@ -91,10 +59,7 @@ def createCheckbox(position, checked=False):
     for y in range(position[1], position[1] + width):
         img.putpixel((x, y), inky_display.BLACK)
 
-  if checked:
-    print('checked')
-  else:
-    print('not checked')
+  if not checked:
     small_width = width - (small_border_width*2)
     small_pos = (position[0] + small_border_width, position[1] + small_border_width)
     for x in range(small_pos[0], small_pos[0] + small_width):
@@ -104,17 +69,12 @@ def createCheckbox(position, checked=False):
 def addItem(text, position, checked=False):
   item_w, item_h = hanken_medium_font.getsize(text)
   draw.text((50, 40 * position + 60), text, inky_display.BLACK, font=hanken_medium_font)
-  createCheckbox((15, 40*position + 70), False)
+  createCheckbox((15, 40*position + 65), checked)
     
-addItem('Go to school', 0)
-addItem('Go to school', 1)
-addItem('Go to school', 2)
-
-print(icons)
-print(masks)
-
-img.paste(icons['full-cb'], (100, 100), masks['full-cb'])
-img.paste(icons['empty-cb'], (50, 50), masks['empty-cb'])
+addItem('Go to school', 0, True)
+addItem('Take a walk', 1, True)
+addItem('Do homework', 2, False)
+addItem('Stop gaming', 3, False)
 
 inky_display.set_image(img)
 inky_display.show()
